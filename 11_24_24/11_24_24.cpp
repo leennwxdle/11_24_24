b0fefca7e10ff7349781af12f1ebdb2caa4f0e55
202410166 <202410166@F1203-19>
2024-11-04 09:33:13 +0800

    Add project files.

---
+++ 11_24_24/11_24_24.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Sample code: Problem Demo 1
+	/*int yearOfBirth;
+	int currentYear;
+	int age;
+
+	cout << "Hi! What is your Year of Birth? ";
+	cin >> yearOfBirth;
+
+	cout << "Input Current Year: ";
+	cin >> currentYear;
+
+		age = currentYear - yearOfBirth;
+
+		cout << "Your age is: " << age;*/
+	
+	// Sample code: Problem Demo 2
+	int php, cad;
+
+	cout << "Input PHP: ";
+	cin >> php;
+
+	cad = php * 0.024;
+
+		cout << "The value of " << php << "php in CAD is " << cad;
+}
